1eea5c8ad09307059565a537aa5ac6ad46eb6a69
Leon Hartley <lhartley97@gmail.com>
2016-10-17 11:10:52 +0100

    libhh initial commit

---
+++ libhh.c
@@ -0,0 +1,11 @@
+//
+// Created by Leon on 17/10/2016.
+//
+
+#include "libhh.h"
+
+int main(int argc, char *argv[]) {
+    printf("[libhh] habbo hotel emulation written in c\n");
+
+    return 0;
+}
---
+++ libhh.h
@@ -0,0 +1,11 @@
+//
+// Created by Leon on 17/10/2016.
+//
+
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]);
+
